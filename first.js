60bb5acb765f9faf94fcaf4b6322f6134ec8942b
ShakirEvan3070 <ausshakirahmed@gmail.com>
2026-05-06 09:07:14 +0600

    First commit

---
+++ first.js
@@ -0,0 +1,13 @@
+// console.log(22)
+// console.log(34)
+var orangePrice = 20;
+var Chocolate = "Catbarry";
+var applePrice = parseInt('20');
+
+var present = true
+
+console.log(typeof orangePrice)
+console.log(typeof Chocolate)
+console.log(typeof applePrice)
+console.log(typeof present)
+console.log(orangePrice + applePrice)
