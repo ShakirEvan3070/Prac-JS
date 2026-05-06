98fc500922968bbcac79ff9c52a24d4dae1eea46
ShakirEvan3070 <ausshakirahmed@gmail.com>
2026-05-06 09:58:41 +0600

    1

--- first.js
+++ first.js
@@ -1,13 +1,68 @@
 // console.log(22)
 // console.log(34)
-var orangePrice = 20;
-var Chocolate = "Catbarry";
-var applePrice = parseInt('20');
+// var orangePrice = 20;
+// var Chocolate = "Catbarry";
+// var applePrice = parseInt('20');
 
-var present = true
+// var present = true
 
-console.log(typeof orangePrice)
-console.log(typeof Chocolate)
-console.log(typeof applePrice)
-console.log(typeof present)
-console.log(orangePrice + applePrice)
+// console.log(typeof orangePrice)
+// console.log(typeof Chocolate)
+// console.log(typeof applePrice)
+// console.log(typeof present)
+// console.log(orangePrice + applePrice)
+
+// let marks = 86;
+
+// let result = marks>=80
+// ? "A+"
+// :marks >=70
+// ?"A"
+// :marks >=60
+// ?"A-"
+// :marks >=50
+// ?"B"
+// :marks >=40
+// ?"B-":"C";
+
+// console.log(result);
+
+// let price = 1050;
+// let isLeader = false
+
+// if(isLeader ===true){
+//     if(price > 1000){
+//         price = price /2
+//     }
+//     else {
+//         price = 300;
+//     }
+// }
+// else {
+//     price = price *2;
+// }
+
+// console.log(price)
+
+// const fruits = ["Apple", "Banana", "Orange", "Mango", "Grapes"];
+
+// for (const fruit of fruits){
+//     console.log("I will eat:")
+//     console.log(fruit)
+// }
+
+const number = 7
+
+if(number > 5){
+    console.log("Number is greater than 5")
+}
+else {
+    console.log("Number is less than 5")
+}
+
+
+
+
+
+
+
